5276ce4483b54999dc3bc86a7979a491665f447a
Gliimu <269347753+Gliimu@users.noreply.github.com>
2026-07-04 02:28:47 +0100

    Delete backend directory

--- backend/middleware/admin.js
+++
@@ -1,8 +0,0 @@
-const adminMiddleware = (req, res, next) => {
-  if (req.user.role !== 'Admin') {
-    return res.status(403).json({ success: false, message: 'Admin access required' });
-  }
-  next();
-};
-
-module.exports = adminMiddleware;
